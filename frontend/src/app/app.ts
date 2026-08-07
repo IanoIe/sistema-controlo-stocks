a6f0fee16991f0210399412c6077749565ec81b9
AC_Marvel <gil_1_10@hotmail.com>
2026-08-07 16:11:37 +0200

    Create Caddyfile, configure Tailwind, and start login page development
    - Add initial Caddyfile configuration
    - Set up Tailwind CSS configuration
    - Start building the login page structure
    - Add initial header and footer components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
